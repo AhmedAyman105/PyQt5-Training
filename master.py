9eadff4433cd91ec6004b1a188c2ae8422e79ba8
Ahmed Ayman <ahmedaymanissa98@gmail.com>
2023-07-26 03:59:14 +0300

    Upload the final project

--- master.py
+++ master.py
@@ -1,6 +1,8 @@
 # Imports
 import sys
+import typing
 from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QWidget
 from PyQt5.uic import loadUi
 import sqlite3
 
@@ -12,10 +14,16 @@ class WelcomeScreen(QtWidgets.QDialog) :
         super(WelcomeScreen,self).__init__()
         loadUi(r'UI\welcomescreen.ui',self)
         self.login.clicked.connect(self.gotologin)
+        self.create_account.clicked.connect(self.gotosignup)
 
     def gotologin(self) :
         login = LoginScreen()
         widget.addWidget(login)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+    
+    def gotosignup(slelf) :
+        signup = SignUP()
+        widget.addWidget(signup)
         widget.setCurrentIndex(widget.currentIndex()+1)
 
 
@@ -52,6 +60,49 @@ class LoginScreen(QtWidgets.QDialog):
             except Exception as e :
                 self.placeholder.setText("User Not Found")
 
+class SignUP(QtWidgets.QDialog):
+    def __init__(self):
+        super(SignUP,self).__init__()
+        loadUi(r'UI\createacc2.ui',self)
+        self.passwordfield.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.confirmpasswordfield.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.signup.clicked.connect(self.gotoprofile)
+        
+    
+    def gotoprofile(self):
+        user = self.emailfield.text().strip()
+        password = self.passwordfield.text().strip()
+        cpassword = self.confirmpasswordfield.text().strip()
+        
+        if len(user) == 0 or len(password) == 0 or (cpassword) == 0 :
+            self.placeholder.setText('Please Fill All Fields')
+        elif password != cpassword :
+            self.placeholder.setText("Password doesn't Match")
+        else:
+            try :
+                conn = sqlite3.connect('data.db')
+                cur = conn.cursor()
+                user_info = [user,password]
+                
+                # Add to data base
+                cur.execute("insert into users (username,password) values (?,?)",user_info)
+
+                conn.commit()
+                conn.close()
+            
+                self.placeholder.setText('')
+                print('Login Success')
+                
+                qpp = Profile()
+                widget.addWidget(qpp)
+                widget.setCurrentIndex(widget.currentIndex()+1)
+            except Exception as e :
+                self.placeholder.setText("User name is not available")
+
+class Profile(QtWidgets.QDialog):
+    def __init__(self):
+        super(Profile,self).__init__()
+        loadUi(r'UI\app.ui',self)
 
 
 # Application
@@ -68,6 +119,7 @@ widget.setFixedWidth(1200)
 
 # add the welcome screen to the stacked widget
 widget.addWidget(welcome)
+widget.setCurrentIndex(0)
 
 # display the widget
 widget.show()
